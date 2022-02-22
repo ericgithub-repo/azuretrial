466add665b21c8e4944fa83943508144e705ff1e
ericgithub-repo <61613283+ericgithub-repo@users.noreply.github.com>
2022-02-22 16:20:16 +0800

    Add project files.

---
+++ PCGApi/Controllers/testinglangController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testinglang.Controllers
+{
+    [Route("pcg/[controller]")]
+    [ApiController]
+    public class testinglangController : ControllerBase
+    {
+        private RepositoryCore.PcgTrialRepository repo;
+        // GET: api/PCG
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            repo = new RepositoryCore.PcgTrialRepository();
+            var result = repo.GetPcgTrial();
+
+            return result;
+        }
+
+        // GET: api/PCG/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PCG
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/PCG/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RepositoryCore/PcgTrialRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace RepositoryCore
+{
+    public class PcgTrialRepository
+    {
+        private string connection = ConfigurationManager.ConnectionStrings["pcgtrialConnection"].ConnectionString;
+        private SqlConnection con;
+        public List<string> GetPcgTrial() {
+            con = new SqlConnection(connection);
+            
+            DataSet ds = new DataSet();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from TrialTable", con);
+            sqlDataAdapter.Fill(ds);
+
+            List<string> ret = new List<string>();
+
+            foreach (DataRow x in ds.Tables[0].Rows)
+            {
+                ret.Add(x[1].ToString());
+            }
+
+            return ret;
+        }
+    }
+}
